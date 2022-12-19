a6478d0d4335112aba5f63916ca99bd271519b85
Nitin Kaul <108913229+Nitin-Kaul-27@users.noreply.github.com>
2022-12-19 16:22:18 -0500

    Add files via upload

---
+++ Multiple_Linear_Regression.py
@@ -0,0 +1,31 @@
+# MULTIPLE LINEAR REGRESSION
+
+# Required Libraries
+import pandas as pd
+import numpy as np
+
+# Loading datset
+data = pd.read_csv("Data.csv")
+print(data.head())
+X = data.iloc[:,2:-1].values
+Y = data.iloc[:,-1].values
+print(X)
+print(Y)
+
+# Splitting the dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, random_state=(0))
+
+# Training the model
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, Y_train)
+
+# Testing the model
+Y_pred = model.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((Y_pred.reshape(len(Y_pred),1), Y_test.reshape(len(Y_test),1)),1))
+
+# Evaluating the model 
+from sklearn.metrics import r2_score
+r2_score(Y_test, Y_pred)
